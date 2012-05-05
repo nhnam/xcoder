7eb0eab96c76e5309993debea32817821b260ae9
Keith Duncan <keith@realmacsoftware.com>
2012-05-05 14:31:14 +0100

    Fix running tests for test host that crashes before starting a test suite

--- lib/xcode/test/parsers/ocunit_parser.rb
+++ lib/xcode/test/parsers/ocunit_parser.rb
@@ -22,83 +22,84 @@ module Xcode
           case piped_row.force_encoding("UTF-8")
           
             when /Run unit tests for architecture '(.*?)' \(GC (.*?)\)/
-              if @currentReport.nil?
-                start_new_report
-              end
               architecture = $1
               garbageCollectionState = $2
-              @currentReport.identifier = File.join architecture, "GC_#{garbageCollectionState}"
+              start_new_report File.join(architecture, "GC_#{garbageCollectionState}")
             
             when /Test Suite '(\S+)'.*started at\s+(.*)/
               name = $1
               time = Time.parse($2)
-              if @currentReport.nil?
-                start_new_report
-              end
               if name=~/\//
-                @currentReport.start
+                current_report.start
               else
-                @currentReport.add_suite name, time
+                current_report.add_suite name, time
               end
-              
+            
             when /Test Suite '(\S+)'.*finished at\s+(.*)./
               time = Time.parse($2)
               name = $1
               if name=~/\//
-                @currentReport.finish
+                current_report.finish
               else
-                @currentReport.in_current_suite do |suite|
+                current_report.in_current_suite do |suite|
                   suite.finish(time)
                 end
               end
             
             when /Test Case '-\[\S+\s+(\S+)\]' started./
               name = $1
-              @currentReport.in_current_suite do |suite|
+              current_report.in_current_suite do |suite|
                 suite.add_test_case name
               end
             
             when /Test Case '-\[\S+\s+(\S+)\]' passed \((.*) seconds\)/
               duration = $2.to_f
-              @currentReport.in_current_test do |test|
+              current_report.in_current_test do |test|
                 test.passed(duration)
               end
             
             when /(.*): error: -\[(\S+) (\S+)\] : (.*)/
               message = $4
               location = $1
-              @currentReport.in_current_test do |test|
+              current_report.in_current_test do |test|
                 test.add_error(message, location)
               end
             
             when /Test Case '-\[\S+ (\S+)\]' failed \((\S+) seconds\)/
               duration = $2.to_f
-              @currentReport.in_current_test do |test|
+              current_report.in_current_test do |test|
                 test.failed(duration)
               end
             
-            # when /failed with exit code (\d+)/, 
+            # when /failed with exit code (\d+)/,
+              
+            
             when /BUILD FAILED/
-              @currentReport.finish
+              current_report.finish
               save_current_report
             
             when /Segmentation fault/
-              @currentReport.abort
+              current_report.abort
               save_current_report
             
             when /Run test case (\w+)/
               # ignore
+            
             when /Run test suite (\w+)/
               # ignore
+            
             when /Executed (\d+) test, with (\d+) failures \((\d+) unexpected\) in (\S+) \((\S+)\) seconds/
               # ignore
-            else
-              return if @currentReport.nil?
             
+            else
+              # ignore if no current report?
+              
+              return if @currentReport.nil?
+              
               @currentReport.in_current_test do |test|
                 test << piped_row
               end
-          
+            
           end # case
           
         end # <<
@@ -109,7 +110,16 @@ module Xcode
         
         private
         
+        def current_report
+          if @currentReport.nil?
+            start_new_report
+          end
+          @currentReport
+        end
+        
         def start_new_report(identifier="")
+          save_current_report
+          
           newReport = Xcode::Test::Report.new do |report|
             report.identifier = identifier
           end
